341d17e118ec4b8f28543b363dfd661bbf08b5b9
wayne <zs577215@gmail.com>
2020-10-31 21:56:45 +0800

    engine-8

--- engine/src/main/java/com/wnzhong/engine/bean/orderbook/GOrderBookImpl.java
+++ engine/src/main/java/com/wnzhong/engine/bean/orderbook/GOrderBookImpl.java
@@ -1,7 +1,6 @@
 package com.wnzhong.engine.bean.orderbook;
 
 import com.google.common.collect.Lists;
-import com.sun.org.apache.xpath.internal.operations.Or;
 import com.wnzhong.engine.bean.command.CmdResultCode;
 import com.wnzhong.engine.bean.command.RbCmd;
 import io.netty.util.collection.LongObjectHashMap;
@@ -17,6 +16,10 @@ import java.util.List;
 import java.util.NavigableMap;
 import java.util.TreeMap;
 
+/**
+ *
+ * @author wayne
+ */
 @Log4j2
 @RequiredArgsConstructor
 public class GOrderBookImpl implements IOrderBook {
@@ -83,6 +86,17 @@ public class GOrderBookImpl implements IOrderBook {
         return CmdResultCode.SUCCESS;
     }
 
+    private void genMatchEvent(RbCmd cmd, OrderStatus status) {
+        long now = System.currentTimeMillis();
+        MatchEvent event = new MatchEvent();
+        event.timestamp = now;
+        event.mid = cmd.mid;
+        event.oid = cmd.oid;
+        event.status = status;
+        event.volume = 0;
+        cmd.matchEventList.add(event);
+    }
+
     private long preMatch(RbCmd cmd, NavigableMap<Long, IOrderBucket> matchingBuckets) {
         int tVol = 0;
         if (matchingBuckets.size() == 0) {
@@ -136,26 +150,50 @@ public class GOrderBookImpl implements IOrderBook {
 
     @Override
     public void fillCode(L1MarketData data) {
-
+        data.code = code;
     }
 
     @Override
-    public void fillSells(int sellSize, L1MarketData data) {
-
+    public void fillSells(int size, L1MarketData data) {
+        if (size == 0) {
+            data.sellSize = 0;
+            return;
+        }
+        int i = 0;
+        for (IOrderBucket bucket : sellBuckets.values()) {
+            data.sellPrices[i] = bucket.getPrice();
+            data.sellVolumes[i] = bucket.getTotalVolume();
+            if (++i == size) {
+                break;
+            }
+        }
+        data.sellSize = i;
     }
 
     @Override
-    public void fillBuys(int buySize, L1MarketData data) {
-
+    public void fillBuys(int size, L1MarketData data) {
+        if (size == 0) {
+            data.buySize = 0;
+            return;
+        }
+        int i = 0;
+        for (IOrderBucket bucket : buyBuckets.values()) {
+            data.buyPrices[i] = bucket.getPrice();
+            data.buyVolumes[i] = bucket.getTotalVolume();
+            if (++i == size) {
+                break;
+            }
+        }
+        data.buySize = i;
     }
 
     @Override
     public int limitSellBucketSize(int maxSize) {
-        return 0;
+        return Math.min(maxSize, buyBuckets.size());
     }
 
     @Override
     public int limitBuyBucketSize(int maxSize) {
-        return 0;
+        return Math.min(maxSize, sellBuckets.size());
     }
 }
